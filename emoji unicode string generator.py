3b9e77261596bbbde117f26a53839a207a47f3eb
Hannah Provenza <hprovenza@gmail.com>
2016-02-12 22:34:25 -0500

    Should have correct ranges now

---
+++ emoji unicode string generator.py
@@ -0,0 +1,24 @@
+__author__ = 'hannahprovenza'
+
+
+
+'''
+        u'\U0001F300-\U0001F64F'
+        u'\U0001F680-\U0001F6FF'
+        u'\u2600-\u26FF\u2700-\u27BF]+',
+'''
+output = []
+# for x in range(2600, 5631):
+  #  output.append(chr(x))
+
+for x in range(127744, 128591):
+    output.append(chr(x))
+
+for x in range(128640, 128709):
+    output.append(chr(x))
+
+for x in range(9728, 10175):
+    output.append(chr(x))
+
+
+print(output)
